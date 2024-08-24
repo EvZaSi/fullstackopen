603daf8031d2e553d89f39d073adf2e3790c1397
Evan Smith <evanzanesmith@gmail.com>
2024-08-23 17:47:41 -0700

    Added frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors');
 const morgan = require('morgan')
 app.use(cors());
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'));
 
